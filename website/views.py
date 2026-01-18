476998b22df52b17eb2fb6521427e3426fd6030c
i-shashikant <shashi873kant@gmail.com>
2026-01-18 23:13:39 +0530

    Fixed

--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint 
+from flask import render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    user = None
+    return render_template("home.html", user=user)
